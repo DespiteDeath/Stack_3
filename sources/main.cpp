0e05e151273e583e59394f23906eb61d20dff3ff
DespiteDeath <Kogut_nikita@mail.ru>
2017-10-11 19:12:08 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,10 +6,15 @@ void foo_pop();
 void foo_empty();
 
 int main() {
-	foo_init();
-	foo_push();
-	foo_pop();
-	foo_empty();
+	try {
+		foo_init();
+		foo_push();
+		foo_pop();
+		foo_empty();
+		} 
+	catch(std::exception &e) {
+		cout << e.what() << endl;
+	}
 
 	return 0;
 }
